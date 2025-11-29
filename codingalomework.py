21e1c1a1da049e685ba5597e3b04a79a15b444ac
Elyon <duboisaryee@gmail.com>
2025-11-29 16:19:08 +0000

    Add conversion from tuple to list
    Convert a tuple of numbers to a list and print it.

--- codingalomework.py
+++ codingalomework.py
@@ -89,5 +89,11 @@ results['final_number'] = secret_number
 
 print(f"You got it! The was {results['final_number']}")
 
+#Tuple --> List
+
+my_tuple = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40)
+my_list = list(my_tuple)
+print(my_list)
+
 
 
